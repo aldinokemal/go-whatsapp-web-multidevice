3f073adb76347407d6317fb36ac07d74551812eb
Aldino Kemal <aldinokemal2104@gmail.com>
2022-02-10 12:01:37 +0700

    fix: move user validation code

--- validations/user_validation.go
+++ validations/user_validation.go
@@ -5,40 +5,27 @@ import (
 	"github.com/aldinokemal/go-whatsapp-web-multidevice/utils"
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/go-ozzo/ozzo-validation/v4/is"
-	"strings"
 )
 
-func ValidateSendMessage(request structs.SendMessageRequest) {
+func ValidateUserInfo(request structs.UserInfoRequest) {
 	err := validation.ValidateStruct(&request,
 		validation.Field(&request.PhoneNumber, validation.Required, is.E164, validation.Length(10, 15)),
-		validation.Field(&request.Message, validation.Required, validation.Length(4, 50)),
 	)
 
 	if err != nil {
 		panic(utils.ValidationError{
 			Message: err.Error(),
-		})
-	} else if !strings.HasPrefix(request.PhoneNumber, "62") {
-		panic(utils.ValidationError{
-			Message: "this is only work for indonesia country (start with 62)",
 		})
 	}
 }
-
-func ValidateSendImage(request structs.SendImageRequest) {
+func ValidateUserAvatar(request structs.UserAvatarRequest) {
 	err := validation.ValidateStruct(&request,
 		validation.Field(&request.PhoneNumber, validation.Required, is.E164, validation.Length(10, 15)),
-		validation.Field(&request.Caption, validation.When(true, validation.Length(4, 200))),
-		validation.Field(&request.Image, validation.Required),
 	)
 
 	if err != nil {
 		panic(utils.ValidationError{
 			Message: err.Error(),
-		})
-	} else if !strings.HasPrefix(request.PhoneNumber, "62") {
-		panic(utils.ValidationError{
-			Message: "this is only work for indonesia country (start with 62)",
 		})
 	}
 }
