d99f543f97f10b64a78b9b274f1aed97db7b2890
Aldino Kemal <aldinokemal2104@gmail.com>
2022-11-06 13:49:30 +0700

    chore: update version

--- src/config/settings.go
+++ src/config/settings.go
@@ -8,7 +8,7 @@ import (
 type Browser string
 
 var (
-	AppVersion             string = "v3.4.1"
+	AppVersion             string = "v3.5.0"
 	AppPort                string = "3000"
 	AppDebug               bool   = false
 	AppOs                  string = fmt.Sprintf("AldinoKemal")
