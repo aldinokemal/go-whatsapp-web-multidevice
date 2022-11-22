2a9a17d3d7a55db01eb57953b17ee34908221c96
Aldino Kemal <aldinokemal2104@gmail.com>
2022-11-22 07:48:52 +0700

    chore: update openapi

--- src/config/settings.go
+++ src/config/settings.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	AppVersion             = "v3.10.0"
+	AppVersion             = "v3.11.0"
 	AppPort                = "3000"
 	AppDebug               = false
 	AppOs                  = fmt.Sprintf("AldinoKemal")
