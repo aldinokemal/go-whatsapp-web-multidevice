7685f39838f0ff05b6b7e3049ca65acda3bd8bbb
Aldino Kemal <aldinokemal2104@gmail.com>
2023-10-11 14:16:48 +0700

    feat: add reply message (#95)
    * feat: add reply message
    
    * feat: add reply message id in UI

--- src/config/settings.go
+++ src/config/settings.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	AppVersion             = "v4.7.4"
+	AppVersion             = "v4.8.0"
 	AppPort                = "3000"
 	AppDebug               = false
 	AppOs                  = fmt.Sprintf("AldinoKemal")
