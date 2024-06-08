9ea5c762bd51c96a08812a073ed9c0e333afe4dc
Abdul Rahman <ar69king@gmail.com>
2024-06-09 02:07:20 +0500

    environment variable support for webhook

--- src/config/settings.go
+++ src/config/settings.go
@@ -3,8 +3,7 @@ package config
 import (
 	"log"
 	"os"
-	"strconv"
-
+	"github.com/joho/godotenv" // Import the godotenv package
 	waProto "go.mau.fi/whatsmeow/binary/proto"
 )
 
@@ -12,7 +11,7 @@ var (
 	AppVersion             = "v4.14.0"
 	AppPort                = "3000"
 	AppDebug               = false
-	AppOs                  = "AldinoKemal"
+	AppOs                  = "ABD-Whatsapp-Client"
 	AppPlatform            = waProto.DeviceProps_PlatformType(1)
 	AppBasicAuthCredential string
 
@@ -23,29 +22,22 @@ var (
 
 	DBName = "whatsapp.db"
 
-	WhatsappAutoReplyMessage    string = getEnv("WhatsappAutoReplyMessage", "")
-	WhatsappWebhook             string = getEnv("WhatsappWebhook", "")
-	WhatsappLogLevel            string = getEnv("WhatsappLogLevel", "ERROR")
-	WhatsappSettingMaxFileSize  int64  = getEnvAsInt64("WhatsappSettingMaxFileSize", 50000000)  // 50MB
-	WhatsappSettingMaxVideoSize int64  = getEnvAsInt64("WhatsappSettingMaxVideoSize", 100000000)  // 100MB
-	WhatsappTypeUser            string = "@s.whatsapp.net"
-	WhatsappTypeGroup           string = "@g.us"
+	WhatsappAutoReplyMessage    string
+	WhatsappWebhook             string
+	WhatsappLogLevel                  = "ERROR"
+	WhatsappSettingMaxFileSize  int64 = 50000000  // 50MB
+	WhatsappSettingMaxVideoSize int64 = 100000000 // 100MB
+	WhatsappTypeUser                  = "@s.whatsapp.net"
+	WhatsappTypeGroup                 = "@g.us"
 )
 
-func getEnv(key, defaultValue string) string {
-	if value, exists := os.LookupEnv(key); exists {
-		return value
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
 	}
-	return defaultValue
-}
-
-func getEnvAsInt64(key string, defaultValue int64) int64 {
-	if valueStr, exists := os.LookupEnv(key); exists {
-		if value, err := strconv.ParseInt(valueStr, 10, 64); err == nil {
-			return value
-		} else {
-			log.Printf("Error parsing environment variable %s: %v", key, err)
-		}
+	WhatsappWebhook = os.Getenv("WhatsappWebhook")
+	if WhatsappWebhook == "" {
+		log.Fatalf("WhatsappWebhook environment variable is not set")
 	}
-	return defaultValue
 }
