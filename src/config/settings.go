67c980849938e73079f59d11bd0e919d52e79c63
Aldino Kemal <aldinokemal2104@gmail.com>
2024-10-11 21:02:47 +0700

    chore: update version

--- src/config/settings.go
+++ src/config/settings.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	AppVersion             = "v4.18.0"
+	AppVersion             = "v4.20.0"
 	AppPort                = "3000"
 	AppDebug               = false
 	AppOs                  = "AldinoKemal"
