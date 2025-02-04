37796b9aec12a5b1495f4072f574929a9dd38ecc
Aldino Kemal <aldinokemal2104@gmail.com>
2025-02-05 05:16:22 +0700

    feat: update version

--- src/config/settings.go
+++ src/config/settings.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	AppVersion             = "v5.0.0"
+	AppVersion             = "v5.1.0"
 	AppPort                = "3000"
 	AppDebug               = false
 	AppOs                  = "AldinoKemal"
