58621e7716ef6a537011eedff9f48849b2596ac0
Aldino Kemal <aldinokemal2104@gmail.com>
2025-07-08 23:52:41 +0700

    chore: upgrade version

--- src/config/settings.go
+++ src/config/settings.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	AppVersion               = "v7.0.0"
+	AppVersion               = "v7.1.0"
 	AppPort                  = "3000"
 	AppDebug                 = false
 	AppOs                    = "AldinoKemal"
