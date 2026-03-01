0010a5277cfaad3656cb816b06c263078c40cf34
aldinokemal <aldinokemal2104@gmail.com>
2026-03-01 13:26:08 +0700

    fix: address nitpicks in event_archive (logging and nil pointer checks)

--- src/infrastructure/whatsapp/event_archive.go
+++ src/infrastructure/whatsapp/event_archive.go
@@ -28,10 +28,19 @@ func handleArchive(ctx context.Context, evt *events.Archive, chatStorageRepo dom
 	chat, err := chatStorageRepo.GetChatByDevice(deviceID, jidStr)
 	if err != nil || chat == nil {
 		// Just log and exit if chat doesn't exist, as it will be synced later
+		if err != nil {
+			logrus.WithError(err).WithField("device_id", deviceID).WithField("jid", jidStr).Debug("Failed to get chat by device in handleArchive")
+		} else {
+			logrus.WithField("device_id", deviceID).WithField("jid", jidStr).Debug("Chat not found in handleArchive, skipping archive status update")
+		}
 		return
 	}
 	
-	isArchived := evt.Action != nil && evt.Action.Archived != nil && *evt.Action.Archived
+	if evt.Action == nil || evt.Action.Archived == nil {
+		return
+	}
+
+	isArchived := *evt.Action.Archived
 
 	chat.Archived = isArchived
 	err = chatStorageRepo.StoreChat(chat)
