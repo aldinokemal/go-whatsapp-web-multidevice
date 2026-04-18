4f04285f4471428d461b14e15fe1a3190c5375d5
Aldino Kemal <aldinokemal2104@gmail.com>
2026-04-18 08:50:49 +0700

    feat: increase version

--- src/config/settings.go
+++ src/config/settings.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	AppVersion             = "v8.3.5"
+	AppVersion             = "v8.4.0"
 	AppPort                = "3000"
 	AppHost                = "0.0.0.0"
 	AppDebug               = false
