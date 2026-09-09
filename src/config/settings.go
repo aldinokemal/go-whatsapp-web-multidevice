e6956e2c3f1de42ca6d9b07e87847ad5d5964b42
Aldino Kemal <aldinokemal2104@gmail.com>
2026-09-09 22:26:47 +0700

    chore(release): v9.3.1

--- src/config/settings.go
+++ src/config/settings.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	AppVersion             = "v9.3.0"
+	AppVersion             = "v9.3.1"
 	AppPort                = "3000"
 	AppHost                = "0.0.0.0"
 	AppDebug               = false
